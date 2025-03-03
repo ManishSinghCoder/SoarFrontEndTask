f7c696fa89e759579f941b6199a397c74fb0b09f
ManishSinghCoder <ms8704479@gmail.com>
2025-03-03 11:42:17 +0530

    Added navbar and sidebar

---
+++ src/component/dashboard/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className="bg-[#F5F7FA] p-5">
+      <h1>Dashboar</h1>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ src/component/sidebar/index.tsx
@@ -0,0 +1,52 @@
+import { Link } from 'react-router-dom'
+import soarIcon from '../../asset/icons/mingcute_task-fill.svg'
+
+interface navItems {
+  icon: string
+  label: string
+  path: string
+}
+
+interface sidebarProps {
+  pathname: string
+  navItems: navItems[]
+}
+
+const SideBar = ({ pathname, navItems }: sidebarProps) => {
+  return (
+    <div className="w-[250px] bg-white h-screen relative left-0">
+      <div className="flex items-center w-[250px] text-gray-400 h-[100px] border-r border-[#E6EFF5] pl-[40px]">
+        <Link to="/" className='flex items-center gap-[10px]'>
+          <img src={soarIcon} className="w-[35px] h-[35px]" alt="soar icon" />
+          <div className="text-[25px] text-[#343c6a] font-extrabold">
+            Soar Task
+          </div>
+        </Link>
+      </div>
+      {navItems.map((item) => (
+        <div key={item.path}>
+          <Link to={item.path}>
+            <div
+              className={`relative flex items-center py-[15px] px-4 py-3 ${pathname === item.path ? 'opacity-100' : 'opacity-30'} hover:opacity-100`}
+            >
+              {pathname === item.path && (
+                <div className="absolute left-0 top-0 h-full w-[5px] bg-[#2d3748] rounded-tr-lg rounded-br-lg"></div>
+              )}
+
+              <img
+                src={item.icon}
+                className={`w-[25px] h-[25px] ml-[35px]`}
+                alt={item.label}
+              />
+              <div className={`ml-3 text-[#232323] `}>
+                {item.label === 'Overview' ? 'Dashboard' : item.label}
+              </div>
+            </div>
+          </Link>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default SideBar
